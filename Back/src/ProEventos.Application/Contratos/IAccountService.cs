586d7c0ccdb738df58ade539096a2385bca75627
Leonardo <leosilvatanabi@gmail.com>
2023-10-31 15:40:36 -0300

    Finalizado implementação de AccountService | Aula.209

---
+++ Back/src/ProEventos.Application/Contratos/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProEventos.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IAccountService
+    {
+        Task<bool> UserExists(string username); //Verifica que Usuario Existe
+        Task<UserUpdateDto> GetUserByUserNameAsync(string username);
+        Task<SignInResult> CheckUserPasswordAsync(UserUpdateDto userUpdateDto, string password);
+        Task<UserDto> CreateAccountAsync(UserDto userDto);
+        Task<UserUpdateDto> UpdateAccount(UserUpdateDto userUpdateDto);
+
+    }
+}
---
+++ Back/src/ProEventos.Persistence/Contratos/IUserPersist.cs
@@ -0,0 +1,19 @@
+﻿using ProEventos.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface IUserPersist : IGeralPersist
+    {
+        //Task do tipo IEnumerable que retornar um User, e ese User vai ser do GetUsersAsync
+        Task<IEnumerable<User>> GetUsersAsync();
+
+        Task<User> GetUserByIdAsync(int id);
+
+        Task<User> GetUserByUserNameAsync(string username);
+    }
+}
